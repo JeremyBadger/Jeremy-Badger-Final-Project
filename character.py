d81ac46f24d6762e259b12f4e0ce0a09aeff54cc
JeremyBadger <47323627+JeremyBadger@users.noreply.github.com>
2019-05-01 08:45:58 -0400

    Update character.py

--- character.py
+++ character.py
@@ -7,13 +7,12 @@ class Character(self):
         self.att = 0
         self.hp = 100
         self.light = 15
-        self.theme = ""
+        global theme
         if self.name == "SOLARIO":
             self.att = 2
             self.hp = 125
-            self.theme = "LIGHT"
+            theme = "LIGHT"
         elif self.name == "LUNA":
             self.att = 1
             self.light = 25
-            self.theme = "NIGHT"
-    
+            theme = "NIGHT"
