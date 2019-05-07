f652aa0a0a45e8eb25d0055cb236e8af45739a74
JeremyBadger <47323627+JeremyBadger@users.noreply.github.com>
2019-05-07 13:44:45 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,18 +3,23 @@ from pygame.locals import *
 FPS = 90
 WHITE = (255,255,255)
 fpsClock = pygame.time.Clock()
-DISPLAYSURF = pygame.display.set_mode((1000,500), 0, 32)
+DISPLAYSURF = pygame.display.set_mode((768,432), 0, 32)
 pygame.display.set_caption("Light")
 SOLARIO = pygame.image.load('resources/SO_igs.png')
 SOLARIO = pygame.transform.scale(SOLARIO,(96,96))
-#pygame.mouse.set_cursor(*pygame.cursors.arrow)
-cursor = pygame.cursors.compile(pygame.cursors.broken_x)
-pygame.mouse.set_cursor(*cursor)
+SELECT = pygame.image.load('resources/chSel.png')
+SELECT = pygame.transform.scale(SELECT,(768,432))
+soSelect = pygame.Rect(129, 147, 222, 237)
+mouse = pygame.Rect(1,1, 5,5)
+mousepos = pygame.mouse.get_pos()
+
 
 while True:
     #Fill in background
     DISPLAYSURF.fill(WHITE)
-    DISPLAYSURF.blit(SOLARIO,(1,1))
+    DISPLAYSURF.blit(SELECT,(1,1))
+    if sprite.collideany(mouse,soSelect):
+        DISPLAYSURF.blit(SOLARIO,(1,1))
 
     #Event loop
     for event in pygame.event.get():
