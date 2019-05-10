3eb8dcc94d49648cdf74022146e8219b604a24aa
JeremyBadger <47323627+JeremyBadger@users.noreply.github.com>
2019-05-10 09:27:02 -0400

    bruh moment

--- character.py
+++ character.py
@@ -1,8 +1,12 @@
 import pygame, sys, time, random
 from pygame.locals import *
+SOLARIO = pygame.image.load('resources/CharEnemy/SO_igs.png')
+SOLARIO = pygame.transform.scale(SOLARIO,(96,96))
 class Character(pygame.sprite.Sprite):
     def __init__(self, name):
         self.name = name
+        self.x = 0
+        self.y = 300
         self.defense = 0
         self.att = 0
         self.hp = 100
@@ -12,6 +16,7 @@ class Character(pygame.sprite.Sprite):
             self.att = 2
             self.hp = 125
             theme = "LIGHT"
+            self.image = SOLARIO
         elif self.name == "LUNA":
             self.att = 1
             self.light = 25
--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 import pygame, sys, time, random
 from pygame.locals import *
 from character import Character
+from platform import Platform
 FPS = 90
 WHITE = (255,255,255)
 fpsClock = pygame.time.Clock()
 DISPLAYSURF = pygame.display.set_mode((768,432), 0, 32)
 pygame.display.set_caption("Light")
-SOLARIO = pygame.image.load('resources/CharEnemy/SO_igs.png')
-SOLARIO = pygame.transform.scale(SOLARIO,(96,96))
 SELECT = pygame.image.load('resources/Backgrounds/chSel.png')
 SELECT = pygame.transform.scale(SELECT,(768,432))
 DAY1 = pygame.image.load('resources/Backgrounds/BetterDBG.png')
@@ -15,8 +14,19 @@ DAY1 = pygame.transform.scale(DAY1,(768,432))
 NIGHT1 = pygame.image.load('resources/Backgrounds/BetterNBG.png')
 NIGHT1 = pygame.transform.scale(NIGHT1,(768,432))
 soSelect = pygame.Rect(129, 147, 222, 237)
+grassp1 = Platform(10, 1, 0, 382, 'resources/Platform-Textures/plat-text-dirt.png')
 stage = "SELECT"
 character = ""
+player = ""
+def plat_detect(entity1, plat):
+    if pygame.sprite.spritecollideany(entity1, plat.top):
+        entity1.rect.y = plat.posY - 96
+    elif pygame.sprite.spritecollideany(entity1, plat.left):
+        entity1.rect.x = plat.posX
+    elif pygame.sprite.spritecollideany(entity1, plat.right):
+        entity1.rect.x = plat.posX + (plat.width * 50)
+    elif pygame.sprite.spritecollideany(entity1, plat.bottom):
+        entity1.rect.y = plat.posY + (plat.height * 50)
 
 while True:
     #Fill in background
@@ -28,6 +38,9 @@ while True:
             DISPLAYSURF.blit(DAY1,(1,1))
         elif player.name == "LUNA":
             DISPLAYSURF.blit(NIGHT1,(1,1))
+    if player != "":
+        DISPLAYSURF.blit(player.image, (player.x, player.y))
+        DISPLAYSURF.blit(grassp1.texture,(grassp1.posX,grassp1.posY))
 
     #Event loop
     for event in pygame.event.get():
